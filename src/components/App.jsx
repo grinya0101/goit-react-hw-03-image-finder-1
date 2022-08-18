b53306e89067126fdf61e6e0d9e1b11eca4424c9
Darya Grygorova <97409600+DaryaGrygorova@users.noreply.github.com>
2022-08-18 18:33:03 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import { GlobalStyle } from './GlobalStyle';
+import { Box } from './Box';
+
+export const App = () => {
+  return (
+    <Box>
+      <div>React homework template</div>
+      <GlobalStyle />
+    </Box>
+  );
+};
---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+import 'modern-normalize';
+
+export const GlobalStyle = createGlobalStyle`
+body {
+  margin: 0;
+  padding: 20px;
+  font-family: 'Roboto', sans-serif;
+  font-size: 24px;
+  color: #010101;}
+
+ul {
+  padding: 0;
+  margin: 0;
+  list-style-type: none; 
+}
+
+h1, h2, h3, p {
+  padding: 0;
+  margin: 0;
+}
+
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+
+`;
