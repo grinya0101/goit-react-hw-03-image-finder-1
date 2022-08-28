40f260f69ac0122c5937de8e12501587d5b894ae
DaryaGrygorova <grygorova.darya@gmail.com>
2022-08-28 23:25:55 +0300

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,23 +3,81 @@ import { Component } from 'react';
 import { Box } from './Box';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
-import { ToastContainer } from 'react-toastify';
+import ImagesApi from '../service-api/ImagesAPI';
+import Loader from './Loader';
+import { Button } from './ImageGallery/ImageGallery.styled';
 
 import { GlobalStyle } from './GlobalStyle';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const initialValues = {
+  searchQuery: '',
+  page: 1,
+  per_page: 40,
+  imagesData: [],
+  total: null,
+  totalHits: null,
+  isLoading: false,
+};
 
 export class App extends Component {
-  state = { searchQuery: '' };
+  state = initialValues;
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.page !== this.state.page ||
+      prevState.searchQuery !== this.state.searchQuery
+    ) {
+      this.setState({ isLoading: true });
+      ImagesApi.getImages(
+        this.state.searchQuery,
+        this.state.page,
+        this.state.per_page
+      )
+        .then(data => {
+          this.setState(prevState => ({
+            imagesData: [...prevState.imagesData, ...data.hits],
+            totalHits: data.totalHits,
+            total: data.total,
+          }));
+
+          if (this.state.page === 1) {
+            toast.success(`Hooray! We found ${data.total} images`);
+          }
+
+          if (
+            this.state.page !== 1 &&
+            this.state.per_page * this.state.page >= this.state.totalHits
+          ) {
+            toast.info(` You've reached the end of search results`);
+          }
+        })
+        .catch(err => toast.error(err))
+        .finally(() => this.setState({ isLoading: false }));
+    }
+  }
 
   onFormSubmit = ({ searchQuery }) => {
     if (searchQuery.trim() === '') {
-      alert('Enter search query');
+      toast.warning('Enter search query');
       return;
     }
-    this.setState({ searchQuery });
+    this.setState({
+      ...initialValues,
+      searchQuery,
+    });
+  };
+
+  onLoadMoreHandler = () => {
+    this.setState(({ page }) => {
+      return { page: (page += 1) };
+    });
   };
 
   render() {
-    const { searchQuery } = this.state;
+    const { imagesData, totalHits, per_page, page, isLoading } = this.state;
+
     return (
       <Box
         display="grid"
@@ -28,8 +86,18 @@ export class App extends Component {
         paddingBottom="24px"
       >
         <Searchbar onFormSubmit={this.onFormSubmit} />
-        <ImageGallery searchQuery={searchQuery} />
+        <ImageGallery imagesData={imagesData} />
 
+        {isLoading ? (
+          <Loader />
+        ) : (
+          !!imagesData?.length &&
+          per_page * page < totalHits && (
+            <Button type="button" onClick={this.onLoadMoreHandler}>
+              Load more
+            </Button>
+          )
+        )}
         <GlobalStyle />
         <ToastContainer autoClose={3000} />
       </Box>
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,71 +2,13 @@ import { Component } from 'react';
 
 import ImageGalleryItem from './ImageGalleryItem';
 import Modal from '../Modal';
-import ImagesApi from '../../service-api/ImagesAPI';
-import Loader from 'components/Loader';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
-import { Gallery, Button } from './ImageGallery.styled.js';
+import { Gallery } from './ImageGallery.styled.js';
 
 class ImageGallery extends Component {
   state = {
-    page: 1,
-    per_page: 40,
-    imagesData: [],
-    total: null,
-    totalHits: null,
     selectedImage: {},
-    isLoading: false,
   };
-
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      !this.state.isLoading &&
-      prevProps.searchQuery !== this.props.searchQuery
-    ) {
-      this.setState({ isLoading: true });
-      ImagesApi.getImages(this.props.searchQuery, 1, this.state.per_page)
-        .then(data => {
-          this.setState({
-            page: 1,
-            imagesData: data.hits,
-            totalHits: data.totalHits,
-            total: data.total,
-          });
-          toast.success(`Hooray! We found ${data.total} images`);
-        })
-        .catch(err => toast.error(err))
-        .finally(() => this.setState({ isLoading: false }));
-    }
-
-    if (
-      prevProps.searchQuery === this.props.searchQuery &&
-      prevProps.searchQuery !== '' &&
-      prevState.page !== this.state.page
-    ) {
-      this.setState({ isLoading: true });
-      ImagesApi.getImages(
-        this.props.searchQuery,
-        this.state.page,
-        this.state.per_page
-      )
-        .then(data => {
-          this.setState(prevState => {
-            const imagesData = [...prevState.imagesData, ...data.hits];
-            return { imagesData };
-          });
-
-          if (this.state.per_page * this.state.page >= this.state.totalHits) {
-            toast.info(` You've reached the end of search results`);
-          }
-        })
-        .catch(err => toast.error(err))
-        .finally(() => {
-          this.setState({ isLoading: false });
-        });
-    }
-  }
 
   onItemClick = selectedImage => {
     this.setState({ selectedImage });
@@ -76,16 +18,9 @@ class ImageGallery extends Component {
     this.setState({ selectedImage: {} });
   };
 
-  onLoadMoreHandler = () => {
-    this.setState(({ page }) => {
-      const newPage = (page += 1);
-      return { page: newPage };
-    });
-  };
-
   render() {
-    const { imagesData, selectedImage, totalHits, per_page, page, isLoading } =
-      this.state;
+    const { selectedImage } = this.state;
+    const { imagesData } = this.props;
     return (
       <>
         <Gallery>
@@ -99,17 +34,6 @@ class ImageGallery extends Component {
             ))}
         </Gallery>
 
-        {isLoading ? (
-          <Loader />
-        ) : (
-          !!imagesData?.length &&
-          per_page * page < totalHits && (
-            <Button type="button" onClick={this.onLoadMoreHandler}>
-              Load more
-            </Button>
-          )
-        )}
-
         {selectedImage?.largeImageURL && (
           <Modal onClose={this.onModalClose}>
             <img src={selectedImage.largeImageURL} alt={selectedImage.tags} />
@@ -120,14 +44,4 @@ class ImageGallery extends Component {
   }
 }
 
-function softScroll() {
-  const { height: cardHeight } = document
-    .querySelector('.gallery')
-    .firstElementChild.getBoundingClientRect();
-
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: 'smooth',
-  });
-}
 export default ImageGallery;
